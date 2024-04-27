ab1fd044d33991b2ccc7a2fbf10cdf5695068a3a
maitri-h1234 <167611490+maitri-h1234@users.noreply.github.com>
2024-04-27 18:04:32 +0530

    Add files via upload

---
+++ frequency.py
@@ -0,0 +1,13 @@
+'''test_string=input("enter string:")
+l=[]
+l=test_string.split()
+wordfreq=[l.count(p) for p in l]
+print(zip(l,wordfreq))
+print(dict(zip(l,wordfreq)))'''
+
+a=[1,2,3]
+b=['a','b','c']
+res=set(zip(a,b))
+print(res)
+
+
